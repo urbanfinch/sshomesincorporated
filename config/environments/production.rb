c3210da327ef99c42c6b102991c236281c2873fa
Aaron Wright <acwrightdesign@gmail.com>
2013-05-07 18:13:50 -0500

    S3 fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Sshomesincorporated::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
+    :url => ':s3_domain_url',
     :default_url => '/assets/missing.png'
   }
 end
